3b4519c7b3e06bbc50ab7166846a7e3a8df99913
Oliver Lean <omlean@gmail.com>
2021-05-27 10:14:38 -0600

    Updated readme and explanatory text on app homepage

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Sniffle
-A streamlit-based app for searching Kaggle's CORD-19 corpus of COVID-19 literature
+Sniffle is an app for searching Kaggle's CORD-19 corpus of COVID-19 literature. It offers the option to search the corpus using different vectorization methods. The current version offers the option to match query to documents by TF-IDF vectorization, or by vectorization based on LDA topic predictions. Currently, both options use cosine distance to find the nearest documents to the query.
+
+Stay tuned for updates that offer more features, including search by word or sentence embeddings and citation network-based recommendation.
+
+View the app deployed on [Streamlit](https://share.streamlit.io/omlean/sniffle/main/streamlit.py).
--- streamlit.py
+++ streamlit.py
@@ -1,37 +1,39 @@
+# import libraries
 import streamlit as st
 import numpy as np
 import pandas as pd
-# import re
 from datetime import datetime
 from scipy.sparse import load_npz
-# from scipy.spatial.distance import cosine
-
 import nltk
 nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
-# from nltk.stem import PorterStemmer
-# from nltk.tokenize import word_tokenize
-# from nltk.corpus import stopwords
-# from nltk.stem import WordNetLemmatizer
 import pickle
-
 from gensim.corpora.dictionary import Dictionary
 from gensim.models.ldamodel import LdaModel
 
+# import custom functions
 from my_functions import *
     
 
 # title
 st.sidebar.title('SNIFFLE')
 num_top_results = st.sidebar.slider("Number of results", min_value=1, max_value=20, value=10, step=1)
-st.sidebar.markdown("""Welcome to Sniffle &#151; a search engine for scientific literature related to the COVID-19 pandemic. It was created as part of my capstone project for the Data Science bootcamp at [Lighthouse Labs](https://www.lighthouselabs.ca/en/data-science-bootcamp).
+st.sidebar.markdown("""Welcome to Sniffle &#151; a search engine for scientific literature related to the COVID-19 pandemic. It's an ongoing project initially created as part of my capstone project for the Data Science bootcamp at [Lighthouse Labs](https://www.lighthouselabs.ca/en/data-science-bootcamp).
 
-This app uses the CORD-19 dataset available on [Kaggle](https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge).
-
-See the [repo](https://github.com/omlean/sniffle) on Github.
+This app uses the CORD-19 dataset available on [Kaggle](https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge). See the [repo](https://github.com/omlean/sniffle) on Github.
 
 > Note: To focus on the most up-to-date information (and to save search time), this version returns documents dated in 2021.
+
+Sniffle allows you to search the corpus using either of two methods: 
+
+1. _TF-IDF vectorization_. This method emphasises the words that are most characteristic of the document relative to the entire corpus, i.e. words that are common in the document and/or rare across the corpus as a whole. 
+2. _LDA Topic vectorization_. This method vectorizes the document/query by representing its predicted topic profile. The topics in question are based on an [LDA](https://en.wikipedia.org/wiki/Latent_Dirichlet_allocation) topic model pre-trained on the corpus.
+
+Each method has different advantages: TF-IDF uses direct string-to-string matching, so it works well for finding documents about specific, named entities like a particular drug or gene (provided that drug or gene is given the same exact name in the document as in the query).
+
+On the other hand, LDA-based searching abstracts from direct word matches and focuses instead on collections of words that tend to be mentioned together in the same documents, and are hence assumed to be semantically related. This makes it less able to pinpoint specific narrow search interests, but is able to find documents which are topically connected in a more general sense. For example, a search for the "effectiveness of remdesivir for treating COVID-19" may return documents about _other_ antiviral treatments &#151; treatments that the user may not be aware of but may nevertheless be interested to know about. This makes it useful for more open-ended, exploratory searching.
+
 """)
 
 # Create a text element and let the reader know the data is loading.
@@ -49,12 +51,16 @@ model = LdaModel.load('data/lda_100_0.01_0.1_model')
 dictionary = Dictionary.load('data/dictionary.dict')
 corpus = MyCorpus(data.search_text.tolist(), dictionary=dictionary)
 
-
 # Notify the reader that the data was successfully loaded.
 data_load_state.text('Loading data...done!')
 
+# text box for user to input query
 query = st.text_area("Enter search query here:", "Evidence for effectiveness of masks in preventing COVID-19 transmission.")
-search_method = st.selectbox('Select search method', ['TF-IDF','LDA Topics'], index=0)
+
+# choice of vectorization method
+search_method = st.selectbox('Select search method.', ['TF-IDF','LDA Topics'], index=0)
+
+# "Search" button
 start_search = st.button("Search")
 
 if start_search:
